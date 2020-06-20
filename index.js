ded62c07b595a0103d9f834bb16a51855098ec2c
fgin69 <fh69mf@gmail.com>
2020-06-20 16:32:43 +0300

    add profile link, add some middleware for status 404 and multer for profile storage

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const path = require('path');
 const csrf = require('csurf');
 const flash = require('connect-flash');
 const mongoose = require('mongoose');
+const helmet = require ('helmet')
 const PORT = process.env.PORT || 3000;
 const exphbs = require('express-handlebars');
 const session = require('express-session');
@@ -14,9 +15,13 @@ const addRoutes = require('./routes/add');
 const coursesRoutes = require('./routes/courses');
 const ordersRoutes = require('./routes/orders');
 const authRoutes = require('./routes/auth');
+const profileRoutes = require('./routes/profile')
 const varMiddleware = require('./middleware/variables');
+const errorHandler = require('./middleware/error')
 const userMiddleware = require('./middleware/user');
-const keys = require('./keys')
+const fileMiddleware = require('./middleware/file')
+const keys = require('./keys');
+const { use } = require('./routes/profile');
 
 const hbs = exphbs.create({
 	defaultLayout: 'main',
@@ -30,6 +35,7 @@ const store = new MongoStore({
 })
 
 app.use(express.static(path.join(__dirname,'public')))
+app.use('/images', express.static(path.join(__dirname, 'images')))
 app.use(express.urlencoded({extended: true}))
 app.use(session({
 	secret: keys.SESSION_SECRET,
@@ -37,20 +43,26 @@ app.use(session({
 	saveUninitialized: false,
 	store
 }))
+app.use(fileMiddleware.single('avatar'))
 app.use(csrf())
 app.use(flash())
+app.use(helmet())
 app.use(varMiddleware)
 app.use(userMiddleware)
 
 app.use('/',homeRoutes)
+
 app.use('/add',addRoutes)
 app.use('/courses',coursesRoutes)
 app.use('/cart', cartRoutes)
 app.use('/orders', ordersRoutes)
 app.use('/auth', authRoutes)
+app.use('/profile', profileRoutes)
 app.engine('hbs', hbs.engine)
 app.set('view engine', 'hbs')
 app.set('views','views')
+
+app.use(errorHandler)
 
 
 async function start(){
